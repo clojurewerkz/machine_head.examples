72a20b878bbe6038393600524698961a12acbb7a
Michael Klishin <michael@defprotocol.org>
2013-10-01 00:39:14 +0400

    Port Blabbr example

---
+++ src/clojurewerkz/machine_head/examples/blabbr.clj
@@ -0,0 +1,25 @@
+(ns clojurewerkz.machine-head.examples.blabbr
+  (:gen-class)
+  (:require [clojurewerkz.machine-head.client :as mh]))
+
+(def ^:const topic "nba.scores")
+
+(defn start-consumer
+  [conn ^String username]
+  (mh/subscribe conn
+                [topic]
+                (fn [^String topic _ ^bytes payload]
+                  (println (format "[consumer] %s received %s" username (String. payload "UTF-8"))))))
+
+(defn -main
+  [& args]
+  (let [id    (mh/generate-id)
+        conn  (mh/connect "tcp://127.0.0.1:1883" id)
+        users ["joe" "aaron" "bob"]]
+    (doseq [u users]
+      (let [conn  (mh/connect "tcp://127.0.0.1:1883" (format "consumer.%s" u))]
+        (start-consumer conn u)))
+    (mh/publish conn topic "BOS 101, NYK 89")
+    (mh/publish conn topic "ORL 85, ALT 88")
+    (Thread/sleep 150)
+    (System/exit 0)))
