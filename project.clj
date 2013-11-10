7d57890884b21b74ec1c19bfc44fc0db60ec1296
Michael Klishin <michael@clojurewerkz.org>
2013-11-10 14:54:39 +0400

    Bump dependency to beta4

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure       "1.5.1"]
-                 [clojurewerkz/machine_head "1.0.0-beta1"]])
+                 [clojurewerkz/machine_head "1.0.0-beta4"]])
