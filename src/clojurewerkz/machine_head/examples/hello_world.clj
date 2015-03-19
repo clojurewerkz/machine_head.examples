18b3b39dd7d2fa4a07774e35cbaaaecc440574ae
Michael Klishin <michael@novemberain.com>
2015-03-19 23:04:52 +0100

    Update hello_world.clj

--- src/clojurewerkz/machine_head/examples/hello_world.clj
+++ src/clojurewerkz/machine_head/examples/hello_world.clj
@@ -6,13 +6,13 @@
   [& args]
   (let [id   (mh/generate-id)
         conn (mh/connect "tcp://127.0.0.1:1883" id)]
-    ; In version clojurewerkz.machine-head <1.0.0beta8 the topic is a list instead of a hashmap
-    ; the following line in 1.0.0-beta8 would look as such below.
-    ;
-    ; (mh/subscribe conn ["hello"] (fn [^String topic _ ^bytes payload]
-    ;                              (println (String. payload "UTF-8"))))
-    ;
-    ; Note: that the 0 in the topic map represents the mqtt QoS value.
+    ;; In version clojurewerkz.machine-head <1.0.0beta8 the topic is a list instead of a hashmap
+    ;; the following line in 1.0.0-beta8 would look as such below.
+    ;;
+    ;; (mh/subscribe conn ["hello"] (fn [^String topic _ ^bytes payload]
+    ;;                              (println (String. payload "UTF-8"))))
+    ;;
+    ;; Note: that the 0 in the topic map represents the mqtt QoS value.
 
     (mh/subscribe conn {"hello" 0} (fn [^String topic _ ^bytes payload]
                                    (println (String. payload "UTF-8"))))
